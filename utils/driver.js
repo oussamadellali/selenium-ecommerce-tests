0f79466f334caf7b6c2005c5d946e3cc24aa7135
Dellali Oussama <dellali.oussama.24@gmail.com>
2026-04-05 17:40:12 +0100

    Enhance Chrome driver options for stability
    Added options for no-sandbox and disable-dev-shm-usage.

--- utils/driver.js
+++ utils/driver.js
@@ -1,9 +1,12 @@
 const { Builder } = require('selenium-webdriver');
 const chrome = require('selenium-webdriver/chrome');
 
+
 async function buildDriver() {
     let options = new chrome.Options();
-    options.addArguments('--headless'); // CI friendly
+    options.addArguments('--headless');
+    options.addArguments('--no-sandbox');
+    options.addArguments('--disable-dev-shm-usage');
 
     return await new Builder()
         .forBrowser('chrome')
